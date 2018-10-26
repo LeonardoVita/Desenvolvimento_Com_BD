d9258e5f58d1f8dd1b042d105696e2efc7942431
LeonardoVita <Leonardo.vita0606@gmail.com>
2018-10-25 22:07:07 -0300

    update

--- Projeto_Mercado/src/DAO/FabricaConexao.java
+++ Projeto_Mercado/src/DAO/FabricaConexao.java
@@ -5,19 +5,45 @@
  */
 package DAO;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Leo
  */
 public class FabricaConexao {
-    
-   private String servidor = "";
-   private String base = "";
-   private String login = "";
-   private String senha = "";
-   
-   public FabricaConexao(){
-       
-   }
-   
+
+    private String servidor = "jdbc:mysql://localhost:3306/";
+    private String banco = "aps_mercado";
+    private String login = "root";
+    private String senha = null;
+
+    public Connection conexao() throws SQLException, ClassNotFoundException {
+
+        Class.forName("com.mysql.jdbc.Driver");
+        
+        return (Connection) DriverManager.getConnection(getServidor()+getBanco()
+                ,getLogin(),getSenha());
+        
+        
+    }
+
+    public String getServidor() {
+        return servidor;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
 }
