03bcd838379a48b81d96dc4e540937b3e12c2d1b
LeonardoVita <Leonardo.vita0606@gmail.com>
2018-10-25 22:06:19 -0300

    update

---
+++ Projeto_Mercado/src/DAO/FabricaConexao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author Leo
+ */
+public class FabricaConexao {
+    
+   private String servidor = "";
+   private String base = "";
+   private String login = "";
+   private String senha = "";
+   
+   public FabricaConexao(){
+       
+   }
+   
+}
